5e734381de90bf560ed7c4bfbce89ae344b22d81
madhurmehta007 <12madhurmehta@gmail.com>
2023-09-16 03:04:38 +0530

    Added Lawyer Appointment Screen

--- app/src/main/java/bharat/law/nyayasetu/client/ui/HomeFragment.kt
+++ app/src/main/java/bharat/law/nyayasetu/client/ui/HomeFragment.kt
@@ -6,19 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import bharat.law.nyayasetu.R
+import bharat.law.nyayasetu.databinding.FragmentHomeBinding
+import bharat.law.nyayasetu.databinding.FragmentLawyerAppointmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding
+        get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         requireActivity().window.setStatusBarColor(ContextCompat.getColor(requireActivity(), R.color.white))
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding.cvLawyerAppointment.setOnClickListener {
+
+        findNavController().navigate(R.id.action_homeFragment_to_lawyerAppointmentFragment)
+        }
+        return binding.root
     }
 
 }
--- app/src/main/java/bharat/law/nyayasetu/client/ui/LawyerAppointmentFragment.kt
+++ app/src/main/java/bharat/law/nyayasetu/client/ui/LawyerAppointmentFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import bharat.law.nyayasetu.R
+import bharat.law.nyayasetu.databinding.FragmentLawyerAppointmentBinding
 
 
 class LawyerAppointmentFragment : Fragment() {
+    private var _binding: FragmentLawyerAppointmentBinding? = null
+    private val binding
+        get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentLawyerAppointmentBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_lawyer_appointment, container, false)
+        binding.btnAppointment.setOnClickListener {
+
+        }
+
+        return binding.root
     }
 
 }
